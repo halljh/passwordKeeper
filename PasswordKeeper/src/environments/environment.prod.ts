7df6686359adbba39fb49204adca0e01e0872328
halljh <halljh@HALLJH-W540.rose-hulman.edu>
2017-03-21 08:45:07 -0400

    added firebase, angularfire, material+flex layout

---
+++ PasswordKeeper/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCI30sKysoLEW17gHIddYGIJiUTrNiPHhE",
+    authDomain: "halljh-passwordkeeper.firebaseapp.com",
+    databaseURL: "https://halljh-passwordkeeper.firebaseio.com",
+    storageBucket: "halljh-passwordkeeper.appspot.com",
+    messagingSenderId: "357279967626"
+  }
+};
---
+++ PasswordKeeper/src/environments/environment.ts
@@ -0,0 +1,15 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCI30sKysoLEW17gHIddYGIJiUTrNiPHhE",
+    authDomain: "halljh-passwordkeeper.firebaseapp.com",
+    databaseURL: "https://halljh-passwordkeeper.firebaseio.com",
+    storageBucket: "halljh-passwordkeeper.appspot.com",
+    messagingSenderId: "357279967626"
+  }
+};
